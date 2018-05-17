9e9dde31fcdc78666d331f1eb4ca90aca8fd7e3a
Sofia Yfantidou <syfantid@csd.auth.gr>
2018-05-17 18:26:00 +0200

    Scrapes movie pages for gross and budget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scrapy import cmdline
 
-#cmdline.execute("scrapy crawl nytimes -o nytimes.json".split())
+# cmdline.execute("scrapy crawl nytimes -o nytimes.json".split())
 
 cmdline.execute("scrapy crawl imdb -o imdb.json".split())
--- nytscraper/spiders/imdb.py
+++ nytscraper/spiders/imdb.py
@@ -28,18 +28,19 @@ def get_actor_id(actor_url):
     return re.findall(regex, actor_url)[0]
 
 def is_valid_year(year):
-    return year.isnumeric() and 1980<int(year) and int(year)<1990
+    # return year.isnumeric() and 1979<int(year) and int(year)<1990
+    return year.isnumeric() and  1999 < int(year) # movies in the 2000's to identify most financially successful movies of the 21st century
 
 
 class imdbSpider(scrapy.Spider):
     name = 'imdb'
     domain = 'www.imdb.com'
     allowed_domains = [domain]
-    start_urls = ["https://www.imdb.com/title/tt0096463/fullcredits/"]
+    start_urls = ["https://www.imdb.com/title/tt0451279/fullcredits"]
 
-
+    # Parsing the movie's full credits page
     def parse(self, response):
-        movie_name = response.xpath('.//h3[@itemprop="name"]/a/text()').extract_first()
+        movie_name = response.xpath('.//h3[@itemprop="name"]/text()').extract_first()
         movie_year = cleanString(response.xpath('.//h3[@itemprop="name"]/span/text()').extract_first().strip())
         movie_year = re.findall('\d+', movie_year ).pop() # get only numbers from year
         # print("Movie Year " + str(movie_year) + " Movie Name " + movie_name)
@@ -49,34 +50,38 @@ class imdbSpider(scrapy.Spider):
                 continue
             role_name = actor.xpath('td[4]/a/text()').extract_first() # Get role names that are linked to an IMDB page
             if role_name == None: # Get role names that are not linked to an IMDB page
-                role_name = cleanString(actor.xpath('td[4]/text()').extract_first().split())
+                role_name = actor.xpath('td[4]/text()').extract_first().strip()
             actor_url = actor.xpath('td[2]/a[1]/@href').extract_first()
             actor_url = actor_url[:actor_url.find('?')]
-            yield {
-                'movie_id': get_movie_id(response.url),  # works
-                'movie_name': movie_name,
-                'movie_year': movie_year,
-                'actor_name': cleanString(actor_name),  # works
-                'actor_id': get_actor_id(actor_url),  # works
-                'role_name': cleanString(role_name)  # works
-            }
+            # yield {
+            #     'movie_id': get_movie_id(response.url),  
+            #     'movie_name': movie_name,
+            #     'movie_year': movie_year,
+            #     'actor_name': cleanString(actor_name),  
+            #     'actor_id': get_actor_id(actor_url),  
+            #     'role_name': cleanString(role_name)  
+            # }
 
-            # es.index(index='imdb',
-            #          doc_type='movies',
-            #          body={
-            #             'movie_id': get_movie_id(response.url), # works
-            #             'movie_name': movie_name,
-            #             'movie_year': movie_year,
-            #             'actor_name': cleanString(actor_name), # works
-            #             'actor_id': get_actor_id(actor_url), # works
-            #             'role_name': cleanString(role_name) # works
-            #          })
+            es.index(index='imdb',
+                     doc_type='movies',
+                     body={
+                        'movie_id': get_movie_id(response.url),
+                        'movie_name': movie_name,
+                        'movie_year': movie_year,
+                        'actor_name': cleanString(actor_name),
+                        'actor_id': get_actor_id(actor_url),
+                        'role_name': cleanString(role_name)
+                     })
 
+            yield response.follow(response.url.replace("/fullcredits", ""), callback=self.parse_movie)
             next_page = actor_url
             # print("Next actor page: " + actor_url + " for actor " + actor_name)
             if next_page is not None:
                 yield response.follow(next_page, callback=self.parse_actor)
 
+
+
+    # Parsing the actor's IMDb page
     def parse_actor(self, response):
         for movie in response.xpath("//*[contains(@class,'filmo-category-section')][1]/div"):
             # get only the films the person is an actor in (not producer, writer, etc.)
@@ -94,5 +99,34 @@ class imdbSpider(scrapy.Spider):
             # print("Next page: " + next_page + " " + movie_year)
             if next_page is not None:
                 yield response.follow(next_page, callback=self.parse)
+
+     # Parsing the movie's IMDb page
+    def parse_movie(self,response):
+        movie_name = cleanString(response.xpath('.//h1[@itemprop="name"]/text()').extract_first())
+        for div_element in response.xpath("//div[contains(@class,'txt-block')]"):
+            try:
+                header = div_element.xpath("h4/text()")[0].extract()
+            except IndexError:
+                header = ""
+            if(header == "Budget:"):
+                budget_amount = cleanString(div_element.css("::text")[2].extract().replace('$',''))
+            elif(header == "Cumulative Worldwide Gross:"):
+                gross_amount = cleanString(div_element.css("::text")[2].extract().replace('$',''))
+        print("-----------------------" + movie_name + "-----------------------")
+        try:
+            budget_amount and gross_amount
+        except NameError:
+            print("Data N/A")
+        else:
+            print("Budget: " + budget_amount + " Gross: " + gross_amount)
+            es.index(index='imdb_budget',
+                     doc_type='movies',
+                     body={
+                         'movie_id': get_movie_id(response.url),  
+                         'movie_name': movie_name,
+                         'budget': budget_amount,  
+                         'gross': gross_amount  
+                     })
+
 
 
